ff8fbf497bde3deeaf65e1f12f506e6b2c7892b9
bahoz123 <76633998+bahoz123@users.noreply.github.com>
2021-02-09 18:34:02 +0300

    Update DarkMan.js

--- DarkMan.js
+++ DarkMan.js
@@ -20,8 +20,8 @@ const dateFormat = require("dateformat");
 var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
-const prefix = "-";
-client.login("");
+const prefix = "p!";
+client.login("ODA3NjM2NzYyNTAzMjgyNzEy.YB64WQ.pb2DR09R_zWlaJ5MwHu7pMqeoZM");
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setStatus("idle");
